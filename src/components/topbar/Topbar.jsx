d725b7f8451f61fd56682c9b6e5809e71b07936a
Jatin Bansal <95169356+jatinbansal21@users.noreply.github.com>
2023-07-18 14:14:58 +0530

    Updated Topbar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">DashAdmin</span>
+          <span className="logo">SpectaDash</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
